80b3358e8b6c4e4566547e863cd8fcc356ec013d
Randall Wilk <wilk.randall@gmail.com>
2023-12-27 09:16:21 -0600

    Remove webhook result view component

--- resources/views/components/partials/webhooks/inactive-badge.blade.php
+++
@@ -1,3 +0,0 @@
-<span class="inline-flex items-center rounded-full bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
-    {{ __('easypost::webhooks.labels.inactive') }}
-</span>
